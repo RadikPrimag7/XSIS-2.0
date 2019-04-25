567f95fc6a4d8a0c5eddd77fc873585e50c74b40
magusalim <magusalim354@gmail.com>
2019-04-25 15:35:07 +0700

    first commit 25/04/2019

---
+++ ngexsis/src/main/java/com/ngexsis/NgexsisApplication.java
@@ -0,0 +1,13 @@
+package com.ngexsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgexsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgexsisApplication.class, args);
+	}
+
+}

0af0dccf499f4b68954360df7bcb4d754989c063
RadikRadik <razzor.vasquez@gmail.com>
2019-04-26 14:03:55 +0700

    model n repo

--- ngexsis/src/main/java/com/ngexsis/model/PelatihanModel.java
+++ ngexsis/src/main/java/com/ngexsis/model/PelatihanModel.java
@@ -1,5 +1,10 @@
 package com.ngexsis.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pelatihan")
 public class PelatihanModel {
 
 }
--- ngexsis/src/main/java/com/ngexsis/repository/PelatihanRepo.java
+++ ngexsis/src/main/java/com/ngexsis/repository/PelatihanRepo.java
@@ -1,5 +1,9 @@
 package com.ngexsis.repository;
 
-public interface PelatihanRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PelatihanRepo extends JpaRepository<PelatihanRepo, Integer> {
 
 }

234db11a8693de470fff5fcc3dc599b3daca2a93
Hatose <harutomo.seto@gmail.com>
2019-05-02 22:05:16 +0700

    logincontroller

--- ngexsis/src/main/java/com/ngexsis/controller/LoginController.java
+++ ngexsis/src/main/java/com/ngexsis/controller/LoginController.java
@@ -5,9 +5,12 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.ngexsis.model.UserModel;
 import com.ngexsis.repository.UserRepo;
 
 @Controller
@@ -16,23 +19,32 @@ public class LoginController {
 	@Autowired
 	private UserRepo repo;
 	
+	private String email;
+	private String abuid;
+	private String abpwd;
+	
 	private Log log=LogFactory.getLog(getClass());
 	
-	@RequestMapping(value="/login/index")
-	public String index(Model model, String error, String logout) {
-		if(error!=null) {
-			model.addAttribute("error","your username and password is invalid");
+	@RequestMapping(value="/login/index", method=RequestMethod.GET)
+	public String index(Model model,@PathVariable(name="id")Long id) {
+		UserModel item=repo.findById(id).orElse(null);
+		model.addAttribute("data",item);
+		if(item==null) {
+			System.out.println("invalid email/password");
+			return "login/index";
 		}
-		if(logout!=null) {
-			model.addAttribute("message","You have been logged out successfully");
+		else if(item.getEmail() == null) {
+			System.out.println("invalid email/password");
+			return "login/index";
 		}
-		return "login/index";
+		else if(item.getEmail() != email && item.getAbpwd()!= abpwd) {
+			System.out.println("invalid email/password");
+			return "login/index";
+		}
+		else {
+			return "login/access";
+		}
+		
 	}
-	
-	
-	public String access() {
-		return "select/access";
-	}
-	
 	
 }
